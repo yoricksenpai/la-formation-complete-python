e124ea2b4cde0568904ba9c9504417dac5c80f73
yoricksenpai <116643216+yoricksenpai@users.noreply.github.com>
2022-10-25 12:26:27 +0100

    Update recuperer_input.py

--- exr-001_input-utilisateur/01-sources/recuperer_input.py
+++ exr-001_input-utilisateur/01-sources/recuperer_input.py
@@ -1,2 +1,5 @@
 # Demander le prénom, la ville de naissance et l'âge de la personne qui utilise le script
 # Afficher ces trois informations
+prenom = input("entrer votre prénom")
+ville_de_naissance = input("entrer votre ville natale")
+age = input("entrer votre age")
